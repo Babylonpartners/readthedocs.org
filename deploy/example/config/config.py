72298df14c06eb6ba5d5c4110e83294edd36bea5
Simon <noisyboiler@googlemail.com>
2018-02-19 15:36:14 +0000

    example deploy scripts

---
+++ deploy/example/config/config.py
@@ -0,0 +1,97 @@
+from readthedocs.settings.base import CommunityBaseSettings
+
+import os
+environ = os.environ
+
+
+class CommunitySettings(CommunityBaseSettings):
+    PRODUCTION_DOMAIN = environ['HOSTNAME']
+    WEBSOCKET_HOST = environ.get('WEBSOCKET_HOST')
+    SESSION_COOKIE_DOMAIN = environ.get('HOSTNAME')
+    
+    SITE_ROOT = environ['ROOT']
+    ES_HOSTS = ['elasticsearch:9200']
+    DEBUG = False
+    
+    STATIC_ROOT = os.path.join(environ['STATIC_ROOT'], 'static')
+    MEDIA_ROOT = os.path.join(environ['STATIC_ROOT'], 'media')
+    STATICFILES_DIRS = [
+        os.path.join(SITE_ROOT, 'readthedocs', 'static'),
+    ]
+
+    STATIC_URL = '/static/'
+    print('settings values')
+    print(STATIC_ROOT)
+    print(STATICFILES_DIRS)
+    print(MEDIA_ROOT)
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': environ['POSTGRES_DB'],
+            'USER': environ['POSTGRES_USER'],
+            'PASSWORD': environ['POSTGRES_PASSWORD'],
+            'HOST': 'postgres',
+            'PORT': 5432,
+        },
+    }
+
+    BROKER_URL = 'redis://redis:6379/0'
+    CELERY_ALWAYS_EAGER = False
+    CELERY_ACCEPT_CONTENT = ['pickle', 'json', 'msgpack', 'yaml']
+    CORS_ORIGIN_REGEX_WHITELIST = ['^.+$']
+    CORS_ALLOW_HEADERS = list(CommunityBaseSettings.CORS_ALLOW_HEADERS) + ['csrftoken']
+    CSRF_COOKIE_SECURE = False
+    CSRF_COOKIE_HTTPONLY = False
+    
+    #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    
+    SLUMBER_USERNAME = 'test'
+    SLUMBER_PASSWORD = 'test'
+
+    @property
+    def LOGGING(self):  # noqa
+        logging = super(CommunityProdSettings, self).LOGGING
+        logging['formatters']['default']['format'] = '[%(asctime)s] ' + self.LOG_FORMAT
+        return logging
+    
+    LOG_FORMAT = '%(name)s:%(lineno)s[%(process)d]: %(levelname)s %(message)s'
+    
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'default': {
+                'format': LOG_FORMAT,
+                'datefmt': '%d/%b/%Y %H:%M:%S',
+            },
+        },
+        'handlers': {
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'formatter': 'default'
+            },
+        },
+        'loggers': {
+            'readthedocs': {
+                'handlers': ['console'],
+                'level': 'DEBUG',
+                'propagate': False,
+            },
+            'django': {
+                'handlers':['console'],
+                'propagate': False,
+                'level':'DEBUG',
+            },
+            '': {
+                'handlers': ['console'],
+                'level': 'DEBUG',
+                'propagate': False,
+            },
+        },
+    }
+    ALLOW_ADMIN = True
+
+CommunitySettings.load_settings(__name__)
---
+++ deploy/example/config/rth-start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+python manage.py collectstatic --noinput
+
+python manage.py makemigrations
+python manage.py migrate
+
+# Create admin:admin user on the first run
+echo "from django.contrib.auth.models import User; user=User.objects.filter(username='admin'); user or User.objects.create_superuser('admin', '$ADMIN_EMAIL', 'admin')" | python manage.py shell
+python manage.py makemessages --all
+python manage.py compilemessages
+
+curl -XPUT http://elasticsearch:9200/readthedocs
+uwsgi --ini $ROOT/uwsgi.ini --http $SERVER_ADDRESS:$SERVER_PORT --static-map /media=/opt/readthedocs.org/static/media --static-map /static=/opt/readthedocs.org/static/static
